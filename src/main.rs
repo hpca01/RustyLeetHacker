d7166c8351c423eb040b454e79e1aaeb6cc18f7f
hpca01 <hirenpatelca@gmail.com>
2020-11-24 16:03:22 -0800

    missing positive #s in array in rust w/constant space and time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod missing_num;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn find_missing_int() {
+    let result = missing_num::find_missing_int(vec![1, 2, 3, 4, 5], 5);
+    assert_eq!(result, 6);
+}
+
+#[test]
+fn find_missing_positive_int_with_negatives() {
+    let result = missing_num::find_missing_int(vec![0, -10, 1, 3, -20], 5);
+    assert_eq![result, 2];
 }
